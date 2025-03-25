42997b8ceac4ff815cd50b44aa61867785408ff2
tejaswiniyalagala123 <yalagalatejaswini@gmail.com>
2025-03-25 12:06:20 +0530

    Git Commit

---
+++ MyExample/src/com/example/myMethod.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class myMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world");
+	}
+
+
+}
